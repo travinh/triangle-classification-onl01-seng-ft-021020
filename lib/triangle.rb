0281733cc71ef9b9a8387caead3c0794ff2d61e8
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-27 02:05:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,18 @@
 class Triangle
   # write code here
   
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :side1, :side2, :side3, :type
   
   def initialize(side1,side2,side3)
     @side1 = side1
     @side2 = side2 
     @side3 = side3
+  end
+  
+  def kind 
+  end
+  
+  class TriangleError < StandardError
   end
   
 end
