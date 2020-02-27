921ed9802553771386fcd5548321a41dad3b6284
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-27 02:12:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,11 @@ class Triangle
   end
   
   def kind 
-    if(@side1 ==0 || @side2==0 || @side3 ==0 || s)
+    if(@side1 ==0 || @side2==0 || @side3 ==0 || (@side1 + @side2
+      )< @side3 || (@side1 + @side3) < @side2 || (@side2 + @side3) < @side1)
+      raise TriangleError
+    elsif (@side1 == @side2 == @side3)
+      @type = 
   end
   
   class TriangleError < StandardError
