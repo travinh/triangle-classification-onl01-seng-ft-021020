4cef32740cc4c9969cdaa669f45ebee51d99ae86
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-27 02:04:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,12 @@
 class Triangle
   # write code here
   
-  attr_accessor :side
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2 
+    @side3 = side3
+  end
   
 end
