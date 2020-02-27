9c2bf80e47f1eba9a299f8566e56af2376887104
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-27 02:17:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   # write code here
   
-  attr_accessor :side1, :side2, :side3, :type
+  attr_accessor :side1, :side2, :side3
   
   def initialize(side1,side2,side3)
     @side1 = side1
@@ -14,11 +14,11 @@ class Triangle
       )<= @side3 || (@side1 + @side3) <= @side2 || (@side2 + @side3) <= @side1)
       raise TriangleError
     elsif (@side1 == @side2 && @side1 == @side3)
-      @type = "equilateral"
+      :equilateral
     elsif (@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
-      @type = "isosceles"
+      :isosceles
     else 
-      @type = "scalene"
+      :scalene
     end
   end
   
