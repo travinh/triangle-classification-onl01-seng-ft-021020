46151c9e35babd00551f3b93aa388a18f19f2d1d
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-27 02:14:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     if(@side1 ==0 || @side2==0 || @side3 ==0 || (@side1 + @side2
       )< @side3 || (@side1 + @side3) < @side2 || (@side2 + @side3) < @side1)
       raise TriangleError
-    elsif (@side1 == @side2 == @side3)
+    elsif (@side1 == @side2 && @side1 == @side3)
       @type = ":equilateral"
     elsif (@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
       @type = ":isosceles"
